3ea2710d7fd387c7ab0be58a9b922855a4f94f1f
Asher <asher.bergtraun@gmail.com>
2021-05-21 11:34:13 -0700

    idevtify java variables with jQuery

---
+++ script.js
@@ -0,0 +1,11 @@
+//connect to HTMl with Ids
+var nineAM = $("#9amtext");
+var tenAM = $("#10amtext");
+var elevenAM = $("#11amtext");
+var twelvePM = $("#12pmtext");
+var onePM = $("#1pmtext");
+var twoPM = $("#2pmtext");
+var threePM = $("#3pmtext");
+var fourPM = $("#4pmtext");
+var fivePM = $("#5pmtext");
+
