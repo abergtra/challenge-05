0e167d5797e9def8ca6f5b09a3854a64f110edcc
Asher <asher.bergtraun@gmail.com>
2021-05-23 18:42:38 -0700

    start up javascript set up to get info from local storage

--- script.js
+++ script.js
@@ -24,3 +24,33 @@ var userInput;
 var hourSpan;
 
 //Page Start Up function
+function startUp() {
+    console.log("Log In Time: " + hour);
+
+    var start9AM = JSON.parse(localStorage.getItem("9AM"));
+    nineAM.val(start9AM);
+  
+    var start10AM = JSON.parse(localStorage.getItem("10AM"))
+    tenAM.val(start10AM);
+    
+    var start11AM = JSON.parse(localStorage.getItem("11AM"))
+    elevenAM.val(start11AM);
+    
+    var start12PM = JSON.parse(localStorage.getItem("12PM"))
+    twelvePM.val(start12PM);
+    
+    var start1PM = JSON.parse(localStorage.getItem("1PM"))
+    onePM.val(start1PM);
+    
+    var start2PM = JSON.parse(localStorage.getItem("2PM"))
+    twoPM.val(start2PM);
+    
+    var start3PM = JSON.parse(localStorage.getItem("3PM"))
+    threePM.val(start3PM);
+   
+    var start4PM = JSON.parse(localStorage.getItem("4PM"))
+    fourPM.val(start4PM);
+    
+    var start5PM = JSON.parse(localStorage.getItem("5PM"))
+    fivePM.val(start5PM);
+  } 
